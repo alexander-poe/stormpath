9a24900e10111dcbd023d7d49f2c7b96d8a71691
Megan Smith <meg.eli.smi@gmail.com>
2016-12-29 19:52:42 -0800

    yelp api call working

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const webpack = require('webpack');
 const config = require('./webpack.config');
 const app = express();
 const compiler = webpack(config);
+const Yelp = require('yelpv3');
+
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath
@@ -17,6 +19,30 @@ app.use(stormpath.init(app, {
   }
 }));
 
+ //================================= Yelp Call
+
+var yelp = new Yelp({
+  app_id: 'USwwY8oe_EPJHgmgNAFJrA',
+  app_secret: '28AP3YcIOadjSNasanXU8pPt0e0JBzH8ar8Ndbdz6MUPzG36OBjIW6fTbhJsRouI'
+});
+ 
+// https://www.yelp.com/developers/documentation/v3/business_search 
+yelp.search({term: 'food', location: 'Merced', limit: 10})
+.then(function (data) {
+    console.log(data);
+})
+.catch(function (err) {
+    console.error('ERROR', err);
+});
+
+// userId: 3242340984509345, 
+//   list: {
+//     bars: {}, 
+//     restaurants: {}
+
+//   }
+
+//================================= Stormpath
 
 app.post('/me', bodyParser.json(), stormpath.loginRequired, function (req, res) {
   function writeError(message) {
@@ -58,6 +84,18 @@ app.post('/me', bodyParser.json(), stormpath.loginRequired, function (req, res) 
   }
 });
 
+app.on('stormpath.ready', function () {
+  app.listen(3000, 'localhost', function (err) {
+    if (err) {
+      return console.error(err);
+    }
+    console.log('Listening at http://localhost:3000');
+  });
+});
+
+
+//============================== bootstrap 
+
 app.get('/css/bootstrap.min.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'build/css/bootstrap.min.css'));
 });
@@ -67,11 +105,3 @@ app.get('*', function (req, res) {
 });
 
 
-app.on('stormpath.ready', function () {
-  app.listen(3000, 'localhost', function (err) {
-    if (err) {
-      return console.error(err);
-    }
-    console.log('Listening at http://localhost:3000');
-  });
-});
--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,43 +1,81 @@
 // import React from 'react';
-import OAuthSimple from 'oauthsimple'
 import 'isomorphic-fetch'
 
-export const GET_DATA_SUCCESS = 'GET_DATA_SUCCESS'
-export const getDataSuccess = data => ({
-  type: GET_DATA_SUCCESS, 
-  data
-})
 
-var consumerKey = 'jyQL9WjBSztxK9Hjec1yrQ';    
-var consumerSecret = 'Ebz4pnMKFZYAUujKU5jNU4U5hPM';
-var token = 'q68AzroqStQAwijZk7w12Q8zrBE_rL6Y';
-var tokenSecret = 'dpCZPXBpGYTcscWoilv82RXbK_c';
+// var Yelp = require('yelpv3'); 
 
+// var yelp = new Yelp ({
+//   app_id: 'USwwY8oe_EPJHgmgNAFJrA',
+//   app_secret: '28AP3YcIOadjSNasanXU8pPt0e0JBzH8ar8Ndbdz6MUPzG36OBjIW6fTbhJsRouI'
+// })
 
- var oauth = new OAuthSimple(consumerKey, tokenSecret)
- 
-   var request = oauth.sign({
-     action: "GET",
-     path: "http://crossorigin.me/https://api.yelp.com/v2/search/",
-     parameters: "name=Coffee Bandits",
-     signatures: {api_key: consumerKey, shared_secret: consumerSecret, access_token: token,
-access_secret: tokenSecret},
+// yelp.search({term: 'food', location: '95348', limit: 10})
+// .then(function(data) {
+//   console.log(data)
+// })
+// .catch(function(err) {
+//   console.error(err)
+// })
 
-   })
+// import oauthSignature from 'oauth-signature'
+// var n = require('nonce')()
+// import request from 'request'
+// import qs from 'querystring'
+// import _ from 'lodash'
 
-export const getData = () => {
-  return dispatch => {
-    return fetch(request.signed_url)
-    .then(response => {
-      if (!response.ok) {
-        const error = new Error(response.statusText)
-        error.response = response
-        throw error
-      }
-       return response.json()
-     })
-      .then(response => response.json())
-      .then(data => console.log(data))
-      .catch(error => console.log("Error:", error))
-     }
-  }
+// export const GET_DATA_SUCCESS = 'GET_DATA_SUCCESS'
+// export const getDataSuccess = data => ({
+//   type: GET_DATA_SUCCESS, 
+//   data
+// })
+
+// var consumerKey = 'jyQL9WjBSztxK9Hjec1yrQ'   
+// var consumerSecret = 'Ebz4pnMKFZYAUujKU5jNU4U5hPM'
+// var token = 'q68AzroqStQAwijZk7w12Q8zrBE_rL6Y'
+// var tokenSecret = 'dpCZPXBpGYTcscWoilv82RXbK_c'
+
+// var httpMethod = 'GET'
+
+// var url = 'https://api.yelp.com/v2/search' 
+
+// var default_parameters = {
+//   location: 'San+Francisco', 
+//   sort: '2'
+// }
+
+// var required_parameters = {
+//   oauth_consumer_key: consumerKey, 
+//   oauth_token: token, 
+//   oauth_nonce: n(), 
+//   oauth_timestamp: n().toString().substr(0,10), 
+//   oauth_signature_method: 'HMAC-SHA1', 
+//   oauth_version: '1.3.1'
+// }
+
+// var parameters = _.assign(default_parameters, required_parameters)
+
+// var signatures = oauthSignature.generate(httpMethod, url, parameters, consumerSecret, tokenSecret, {
+//   encodeSignature: false})
+
+// parameters.oauth_signature = signatures
+
+// var paramURL = qs.stringify(parameters)
+
+// var apiURL = url+'?'+paramURL
+
+// export const getData = () => {
+//   return dispatch => {
+//     return fetch(apiURL)
+//     .then(response => {
+//       if (!response.ok) {
+//         const error = new Error(response.statusText)
+//         error.response = response
+//         throw error
+//       }
+//        return response.json()
+//      })
+//       .then(response => response.json())
+//       .then(data => console.log(data))
+//       .catch(error => console.log("Error:", error))
+//      }
+//   }
